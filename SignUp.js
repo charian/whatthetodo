7a74e4cf62fda914e658e59c9c3a968084b04ac2
angdry <charian@wisetracker.co.kr>
2019-10-21 17:52:24 +0900

    styled init

--- SignUp.js
+++ SignUp.js
@@ -1,8 +1,16 @@
 // SignUp.js
 import React from 'react';
-import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+} from 'react-native';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
+import {Button, InputItem} from '@ant-design/react-native';
+import styled from 'styled-components';
 
 export default class SignUp extends React.Component {
   state = {email: '', password: '', errorMessage: null};
@@ -15,20 +23,28 @@ export default class SignUp extends React.Component {
     //   .catch(error => this.setState({errorMessage: error.message}));
     //console.log('handleSignUp' + this.state.email + this.state.password);
   };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Sign Up</Text>
+        <StyledText>Sign Up</StyledText>
         {this.state.errorMessage && (
           <Text style={{color: 'red'}}>{this.state.errorMessage}</Text>
         )}
-        <TextInput
+        <InputItem
+          clear
+          value={this.state.email}
+          onChange={email => this.setState({email})}
+          placeholder="Input Login Email"
+          type="email"
+        />
+        {/* <TextInput
           placeholder="Email"
           autoCapitalize="none"
           style={styles.textInput}
           onChangeText={email => this.setState({email})}
           value={this.state.email}
-        />
+        /> */}
         <TextInput
           secureTextEntry
           placeholder="Password"
@@ -37,15 +53,29 @@ export default class SignUp extends React.Component {
           onChangeText={password => this.setState({password})}
           value={this.state.password}
         />
-        <Button title="Sign Up" onPress={this.handleSignUp} />
+        <StyledBtn onPress={this.handleSignUp} title="Sign Up">
+          <StyledText>Sign Up</StyledText>
+        </StyledBtn>
         <Button
-          title="Already have an account? Login"
           onPress={() => this.props.navigation.navigate('Login')}
-        />
+          type="primary">
+          Already have an account? Login
+        </Button>
       </View>
     );
   }
 }
+
+const StyledBtn = styled.TouchableHighlight`
+  font-weight: bold;
+  border-radius: 50px;
+  background-color: palevioletred;
+  padding: 10px 20px;
+`;
+const StyledText = styled.Text`
+  font-size: 50px;
+  color: #fff;
+`;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
